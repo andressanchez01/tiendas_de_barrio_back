f3850b0dd41e5d848eefa90f952bde58b6240c22
Andrés Sánchez <andres_sanchez01@hotmail.com>
2024-06-20 18:21:39 -0500

    mailer configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "shop_production"
 
   #config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'tiendasdebarrioback-production.up.railway.app' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
